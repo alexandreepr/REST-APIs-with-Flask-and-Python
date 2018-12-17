888f4623efbd4be045a93167afeccb3ca1dd7a24
Alexandre <alexandre@MacBook-Pro-de-Alexandre.local>
2018-12-17 11:34:39 -0200

    creating application endpoints

--- Third Section/app.py
+++ Third Section/app.py
@@ -2,8 +2,45 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/') # site home page
-def home():
-	return "Hello, world"
+stores = [
+	{
+		'name': 'My Wonderful Store',
+		'items': [
+			{
+			'name': 'My item',
+			'price': 15.99
+			}
+		]
+	}
+]
+
+# POST - used to receive data
+# GET - used to send data back only
+
+
+# POST /store data: {name:}
+@app.route('/store', methods=['POST'])
+def create_store():
+	pass
+
+# GET /store/<string:name>
+@app.route('/store/<string:name>')
+def get_store(name):
+	pass
+
+# GET /store
+@app.route('/store')
+def get_stores():
+	pass
+
+# POST /store/<string:name>/item{name:, price:}
+@app.route('/store/<string:name/item', methods=['POST'])
+def create_item_in_store(name):
+	pass
+
+# GET /store/<string:name>/item
+@app.route('/store/<string:name>/item')
+def get_item_in_store():
+	pass
 
 app.run(port=5000)
